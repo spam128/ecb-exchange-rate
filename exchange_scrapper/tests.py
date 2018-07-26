777e380cc09d6c21822c6e39de234a5eea0a2b44
corpuscallosum <corpuscallosum@localhost.localdomain>
2018-07-26 16:20:03 +0200

    test fixed

--- exchange_scrapper/tests.py
+++ exchange_scrapper/tests.py
@@ -1,17 +1,19 @@
 from django.test import TestCase
-from unittest.mock import patch, MagicMock
-
-raw_html = '<ul class="zebraList"><li><a class="rss" href="/rss/fxref-usd.html">US dollar (USD)</a></li>'
+from unittest.mock import patch
+from exchange_scrapper.models import ExchangeRate
 
 
 class ExchangeRateTest(TestCase):
 
     @patch('exchange_scrapper.views.feedparser.parse')
     def test_save_exchange_rate(self, mock_feedparse):
-        feeds = MagicMock()
-        feeds['entries'][0]['cb_exchangerate'] = '1.1690 EUR'
-        mock_feedparse.side_effect = lambda x: feeds
-        resp = self.client.post('/exchange/create/', {'exchange': 'USD'})
+        feeds = {'entries': [{'cb_exchangerate': '1.1690 EUR', 'updated': '2018-07-26T14:15:00+01:00'}]}
+        mock_feedparse.return_value = feeds
+        self.assertEqual(ExchangeRate.objects.count(), 0)
+        resp = self.client.post('/exchange/create/', {'name': 'USD'})
         self.assertEqual(resp.status_code, 200)
-        self.assertTemplateUsed(resp, 'index.html')
-        self.assertContains(resp, '1.1690')
+        self.assertTemplateUsed(resp, 'exchange_scrapper/object_created.html')
+        self.assertEqual(ExchangeRate.objects.count(), 1)
+        exchange_rate = ExchangeRate.objects.first()
+        self.assertEqual(exchange_rate.value, 1.169)
+        self.assertEqual(exchange_rate.name, 'USD')
