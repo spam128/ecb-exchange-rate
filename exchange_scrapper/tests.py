017e7c14e8c9e013babc5e8ba4e313d414bf038c
corpuscallosum <corpuscallosum@localhost.localdomain>
2018-07-26 14:41:52 +0200

    fixed test

--- exchange_scrapper/tests.py
+++ exchange_scrapper/tests.py
@@ -1,20 +1,20 @@
 from django.test import TestCase
-from unittest.mock import patch, Mock
+from unittest.mock import patch, MagicMock
 from .models import ExchangeRate, ExchangeRSS
 
 raw_html = '<ul class="zebraList"><li><a class="rss" href="/rss/fxref-usd.html">US dollar (USD)</a></li>'
 
 class ExchangeRateTest(TestCase):
 
-    @patch('exchange_scrapper.views.requests.get')
-    def test_get_exchange_rate(self, mock_get):
-        mock_html = Mock()
-        mock_html.text = raw_html
-        mock_get.return_value = mock_html
-        resp = self.client.get('/exchange/usd/')
-        self.assertEqual(resp.status_code, 200)
-        self.assertEqual(ExchangeRate.objects.count(), 1)
 
-    def test_save_exchange_rate(self):
-        resp = self.client.post('/exchange/usd/', {})
+
+    @patch('exchange_scrapper.views.feedparser.parse')
+    def test_save_exchange_rate(self, mock_feedparse):
+        feeds = MagicMock()
+        feeds['entries'][0]['cb_exchangerate'] = '1.1690 EUR'
+        mock_feedparse.side_effect = lambda x: feeds
+        resp = self.client.post('/exchange/create/', {'exchange': 'USD'})
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'index.html')
+        self.assertContains(resp, '1.1690')
 
